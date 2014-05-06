fcf0a0fb5ee4ee97a9bbdbef6341a9a994577686
kamal <kamaldaniels@gmail.com>
2014-05-06 14:43:56 +0100

    removed AJAX url

--- kon/urls.py
+++ kon/urls.py
@@ -11,7 +11,6 @@ urlpatterns = patterns('',
     url(r'^googlesignup/$', 'game.views.google_signup', name='google_signup'),
 
     url(r'^game/(?P<pk>\d+)/$', 'game.views.game_detail', name='game_detail'),
-    url(r'^mymove/(?P<pk>\d+)/$', 'game.views.my_move', name='mymove'),
 
     url(r'^invite/$', 'game.views.new_invitation', name='invite'),
     url(r'^invitation/(?P<pk>\d+)/$', 'game.views.accept_invitation', name='accept_invitation'),
